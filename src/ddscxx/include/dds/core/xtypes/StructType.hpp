52458809062dcfb0b349fae8f029fb2764af3078
xiaobo.yang <xiaobo.yang@siemens.com>
2023-04-26 16:40:46 +0800

    StructType.hpp   - Uses the detail::StructType replace the TStructType<detail::StructType> whcih makes the DynamicType could convert to StructType through static_cast. detail/StructType.hpp   - Add the implementation for detail::StructType.

--- src/ddscxx/include/dds/core/xtypes/StructType.hpp
+++ src/ddscxx/include/dds/core/xtypes/StructType.hpp
@@ -18,7 +18,6 @@
 #ifndef OMG_DDS_CORE_XTYPES_STRUCT_TYPE_HPP_
 #define OMG_DDS_CORE_XTYPES_STRUCT_TYPE_HPP_
 
-#include <dds/core/xtypes/TStructType.hpp>
 #include <dds/core/xtypes/detail/StructType.hpp>
 
 namespace dds
@@ -27,7 +26,7 @@ namespace core
 {
 namespace xtypes
 {
-typedef TStructType<detail::StructType> StructType;
+typedef detail::StructType StructType;
 }
 }
 }
--- src/ddscxx/include/dds/core/xtypes/detail/StructType.hpp
+++ src/ddscxx/include/dds/core/xtypes/detail/StructType.hpp
@@ -12,25 +12,52 @@
 #ifndef OMG_DDS_CORE_XTYPES_DETAIL_STRUCT_TYPE_HPP_
 #define OMG_DDS_CORE_XTYPES_DETAIL_STRUCT_TYPE_HPP_
 
+#include <dds/core/xtypes/DynamicType.hpp>
+#include <dds/core/xtypes/MemberType.hpp>
 
 namespace dds {
   namespace core {
     namespace xtypes {
       namespace detail {
+        class StructType : public xtypes::DynamicType {
+          public:
+            StructType(const std::string& name)
+              :  xtypes::DynamicType(name, xtypes::TypeKind::STRUCTURE_TYPE), parent_(nullptr)
+            {
+            }
 
+            StructType(const std::string& name, const StructType& parent, const std::vector<MemberType>& members)
+              : xtypes::DynamicType(name, xtypes::TypeKind::STRUCTURE_TYPE), parent_(&parent), members_(members)
+            {
+            }
 
-        class StructType { };
-        template <typename T>
-        bool isFinal(const TStructType<T>& s) { return false; }
+            template <typename MemberIter>
+            StructType(const std::string& name, const StructType& parent, MemberIter begin, MemberIter end)
+              : xtypes::DynamicType(name, xtypes::TypeKind::STRUCTURE_TYPE), parent_(&parent)
+            {
+              members_.reserve(members_.size() + (end - begin));
+              for (auto it = begin; it != end; it++)
+                members_.emplace_back(*it);
+            }
 
-        template <typename T>
-        bool isExtensible(const TStructType<T>& s) { return false; }
+          public:
+            bool has_parent() const { return this->parent_ == nullptr; }
+            void parent(const StructType& parent) { this->parent_ = &parent; }
+            const StructType& parent() const { return *(this->parent_); }
+            void add_member(const MemberType& member) { this->members_.emplace_back(member); }
 
-        template <typename T>
-        bool isMutable(const TStructType<T>& s) { return false; }
+          private:
+            const StructType* parent_;
+            std::vector<MemberType> members_;
+        };
 
-        template <typename T>
-        bool isNested(const TStructType<T>& s) { return false; }
+        bool isFinal(const StructType& s) { return false; }
+
+        bool isExtensible(const StructType& s) { return false; }
+
+        bool isMutable(const StructType& s) { return false; }
+
+        bool isNested(const StructType& s) { return false; }
       }
     }
   }
